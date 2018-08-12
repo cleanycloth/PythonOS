a550e0ba5524326026c7e1c17e5cd82abbd80926
cleanycloth <aidan.rayner@gmail.com>
2018-08-12 21:17:08 +0100

    Slight modification to Scandisk to capitalise the "D" in ScanDisk, to match the real ScanDisk.

--- scandisk.py
+++ scandisk.py
@@ -7,7 +7,7 @@ init()
 from colorama import Fore, Style, Back
 if osdetect() == "Windows":
     system('color 1f'), sleep(0.05)
-print(Fore.CYAN + Back.BLUE + Style.BRIGHT + "\n\n    RotoWare Scandisk"), sleep(0.01)
+print(Fore.CYAN + Back.BLUE + Style.BRIGHT + "\n\n    RotoWare ScanDisk"), sleep(0.01)
 print("    ----------------------------------------------------------------------"), sleep(0.01)
 if not "1" in open("safeshutdown.txt").read():
     print("\n    Because PythonOS was not properly shut down,"), sleep(0.01)
@@ -15,10 +15,10 @@ if not "1" in open("safeshutdown.txt").read():
     print(Fore.WHITE + Style.NORMAL + "\n    To avoid seeing this message again, always shut down"), sleep(0.01)
     print("    your computer by typing \"exit\" or \"shutdown\""), sleep(0.01)
     print("    at the command line."), sleep(0.01)
-    print("\n    Scandisk is now checking drive C for errors:"), sleep(0.01)
+    print("\n    ScanDisk is now checking drive C for errors:"), sleep(0.01)
     print("\n\n\n\n\n\n\n")
 else:
-    print(Fore.WHITE + Style.NORMAL +"\n    Scandisk is now checking drive C for errors:"), sleep(0.01)
+    print(Fore.WHITE + Style.NORMAL +"\n    ScanDisk is now checking drive C for errors:"), sleep(0.01)
     print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
 print(Fore.CYAN + Style.BRIGHT +"    ----------------------------------------------------------------------"), sleep(0.5)
 print("      " + "0" + "% complete" + Fore.YELLOW + "      ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒", end='\r'), sleep(1.5)
@@ -33,7 +33,7 @@ if osdetect() == "Windows":
 else:
     system('clear')
 error = randint(1,51)
-print(Fore.CYAN + "\n\n    RotoWare Scandisk"), sleep(0.05)
+print(Fore.CYAN + "\n\n    RotoWare ScanDisk"), sleep(0.05)
 print("    ----------------------------------------------------------------------"), sleep(0.01)
 if error == 48:
     print(Fore.WHITE + Style.NORMAL + "\n    Drive C: scanned sucessfully. Some errors were found and repaired."), sleep(0.01)
